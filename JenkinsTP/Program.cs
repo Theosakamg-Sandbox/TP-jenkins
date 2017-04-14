23edabf254396116879a0720b4108e2e7774882a
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2017-04-14 16:55:05 +0200

    Added unit Tests

--- JenkinsTP/Program.cs
+++ JenkinsTP/Program.cs
@@ -2,11 +2,21 @@
 
 namespace JenkinsTP
 {
-	class MainClass
+	public class Program
 	{
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
+		}
+
+		public int Add(int a, int b)
+		{
+			return a + b;
+		}
+
+		public int Div(int a, int b)
+		{
+			return a / b;
 		}
 	}
 }
---
+++ JenkinsTest/Test.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using JenkinsTP;
+
+
+namespace JenkinsTest
+{
+	[TestFixture ()]
+	public class Test
+	{
+		int a = 12;
+		int b = 2;
+
+		[Test ()]
+		public void TestAdd()
+		{
+			Program app = new Program ();
+
+			int result = app.Add (a, b);
+
+			Assert.AreEqual (a + b, result);
+		}
+
+		[Test ()]
+		public void TestDiv()
+		{
+			Program app = new Program ();
+
+			int result = app.Div (a, b);
+
+			Assert.AreEqual (a / b, result);
+		}
+	}
+}
+
